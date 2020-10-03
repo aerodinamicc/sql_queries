bc1e6db12fa1fcd09f884ffc07c4c976b90f9719
z <aerodinamicbg@gmail.com>
2020-10-04 00:36:27 +0300

    custom search

--- real_estate_db/quarters.sql
+++ real_estate_db/quarters.sql
@@ -33,3 +33,47 @@ and area > 0
 and not regexp_like(place, '^с.')
 group by 1, 2
 order by 1, 11
+
+
+
+WITH stats AS (
+select 
+	place,
+	type,
+	approx_percentile(round(price/area), 0.5) as median_price_sqm
+from real_estate_db.daily 
+where measurement_day = (select max(measurement_day) from real_estate_db.daily)
+and is_apartment 
+and is_for_sale 
+and regexp_like(place, 'изгрев|^младост|брези$|^дружба|иван вазов|^изток|слатина|лозенец|хаджи димитър$|оборище|хладилника$|^център|яворов')
+and price > 0
+and area > 0
+and not regexp_like(place, '^с.')
+group by 1, 2
+)
+SELECT 
+	d.place,
+	d.type,
+	d.area,
+	d.price,
+	round(d.price/d.area) AS price_sqm,
+	round(d.price/d.area)-s.median_price_sqm as below_median_sqm,
+	s.median_price_sqm as median,
+	d.details,
+	d.link
+from real_estate_db.daily as d 
+inner join stats s 
+	on s.place = d.place 
+	and s.type = d.type 
+	and s.median_price_sqm >= round(d.price/d.area)
+where measurement_day = (select max(measurement_day) from real_estate_db.daily)
+and is_apartment 
+and is_for_sale 
+and regexp_like(d.place, 'изгрев|^младост|брези$|^дружба|иван вазов|^изток|слатина|дианабад|лозенец|хладилника$|яворов|дианабад|мусагеница|дървеница')
+and price > 0 and price < 110000
+and area > 0
+and not regexp_like(d.place, '^с.')
+order by 6
+
+SELECT * FROM real_estate_db.daily 
+limit 10
